4ca03cc30f9bfb62231607fd83d87125df509448
24b01011314priyanshu <priyanshuniboriya22@gmail.com>
2025-12-15 00:36:17 +0530

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -53,7 +53,7 @@ export default async function handler(req, res) {
         "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
       },
       body: JSON.stringify({
-        model: "llama3-70b-8192",
+       model: "llama3-8b-8192",,
         messages: [{ role: "user", content: prompt }],
         temperature: 0.15,
         max_tokens: 900
